81278a14319112bed69b18ccef6f15f01f0b3402
leoOss <s0564564@htw-berlin.de>
2022-02-02 06:29:44 +0100

    FIXED structure issues and false imports.

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
 from flask import Flask
-#from pymongo import PyMongo
-#from bson.json_util import dumps
+from pymongo import PyMongo
+from bson.json_util import dumps
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://hgaertner:nUH8shJg@reagent1.f4.htw-berlin.de:27017/examples?authSource=examples"
-#mongo = PyMongo(app)
+mongo = PyMongo(app)
 
 @app.route("/<collection_str>", methods=["GET"])
 def get_collection(collection_str):
 
-    # data = mongo.db[collection_str].find({})
+    data = mongo.db[collection_str].find({})
 
-    # if data is None:
-    #     return ("No data found.", 404)
-    # else:
-    #     response = app.response_class(
-    #         response= dumps(data),
-    #         status= 200,
-    #         mimetype='application/json'
-    #     )
+    if data is None:
+        return ("No data found.", 404)
+    else:
+        response = app.response_class(
+            response= dumps(data),
+            status= 200,
+            mimetype='application/json'
+        )
 
-    #     return response
-    return 'Flask app name'
+        return response
 
 @app.route("/")
 def hello_world():
